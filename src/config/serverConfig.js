512471381772f24e5a4f745bff722d2ea5598d7c
tarun291 <tarunkumar222020@gmail.com>
2023-01-14 18:19:42 +0530

    Added RABBIT  MQ config

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -4,5 +4,8 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT,
     EMAIL_ID:process.env.EMAIL_ID,
-    EMAIL_PASSWORD:process.env.EMAIL_PASSWORD
+    EMAIL_PASSWORD:process.env.EMAIL_PASSWORD,
+    MESSAGE_BROKER_URL:process.env.MESSAGE_BROKER_URL,
+    EXCHANGE_NAME:process.env.EXCHANGE_NAME,
+    REMINDER_BINDING_KEY:process.env.REMINDER_BINDING_KEY
 };
